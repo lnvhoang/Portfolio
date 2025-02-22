b82d5535c842d15556d9ca5755d159e17cf2e685
HoangLe4613 <lehoang4613@gmail.com>
2025-02-22 18:30:37 +0700

    commit add code home page

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-console.log("Welcome to the website!");
+const operationSuggestion = document.getElementById("operation-suggestion");
+const computerScreen = document.getElementById("computer-screen");
+let roll = {
+  level: 0, // 2 is Maximum
+  isRollDown: false,
+};
+
+// Mouse event
+document.addEventListener("mousemove", (event) => {
+  updateMousePosition(event.clientX, event.clientY);
+});
+
+// Mouse in screen
+document.addEventListener("mouseenter", (event) => {
+  updateMousePosition(event.clientX, event.clientY);
+  operationSuggestion.style.display = "block";
+  setTimeout(() => {
+    operationSuggestion.style.animation =
+      "show-operation-suggestion 0.5s ease-out forwards";
+  }, 2000);
+});
+
+// Mouse out screen
+document.addEventListener("mouseleave", () => {
+  operationSuggestion.style.opacity = 0;
+  operationSuggestion.style.display = "none";
+});
+function updateMousePosition(x, y) {
+  operationSuggestion.style.transform = `translate(${x + 10}px, ${y + 10}px)`;
+}
+
+// Scroll event
+document.addEventListener("wheel", function (event) {
+  // DeltaY > 0 is roll down
+  if (event.deltaY > 0 && roll.level < 2) {
+    roll.level += 1;
+    roll.isRollDown = true;
+  }
+
+  // DeltaY < 0 is roll up
+  if (event.deltaY < 0 && roll.level > 0) {
+    roll.level -= 1;
+    roll.isRollDown = false;
+  }
+
+  // Roll down to open
+  if (roll.isRollDown) {
+    if (roll.level == 1) {
+      computerScreen.style.animation =
+        "move-transform 0.2s ease-out forwards, move-center 0.3s ease-out forwards";
+    }
+    if (roll.level == 2) {
+      computerScreen.style.animation =
+        "move-full-screen 0.2s ease-out forwards";
+    }
+  } else {
+    if (roll.level == 0) {
+      computerScreen.style.animation =
+        "move-center-back 0.3s ease-out forwards";
+      setTimeout(() => {
+        computerScreen.style.animation =
+          "move-transform-back 0.2s ease-out forwards";
+      }, 300);
+    }
+    if (roll.level == 1) {
+      computerScreen.style.animation =
+        "move-full-screen-back 0.2s ease-out forwards";
+    }
+  }
+});
+
+// Think animate
+function triggerThink() {
+  const bubble = document.getElementById("think");
+  bubble.style.animation = "none";
+  bubble.offsetHeight;
+  bubble.style.animation = "think-animate 5s ease-out";
+  setTimeout(() => {
+    bubble.style.opacity = "1";
+  }, 10);
+  setTimeout(triggerThink, 7000);
+}
+triggerThink();
